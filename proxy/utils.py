da3d3f04b87bc5e104b4a50dddf27f95d384eb6f
amirmohammad pirhosseinloo <amir.hosseinloo@cafebazaar.ir>
2020-11-15 01:25:03 +0330

    Function to create a mattermostdriver

--- proxy/utils.py
+++ proxy/utils.py
@@ -2,6 +2,7 @@ import base64
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
 from django.conf import settings
+from mattermostdriver import Driver
 
 
 def decrypt(text):
@@ -9,3 +10,20 @@ def decrypt(text):
     key = RSA.importKey(settings.VERIFYING_KEY)
     cipher = PKCS1_OAEP.new(key)
     return cipher.decrypt(b64_string)
+
+
+def get_driver(username, password):
+    return Driver({
+        'url': settings.MATTERMOST_API_ENDPOINT,
+        'login_id': username,
+        'password': password,
+        'scheme': 'http',
+        'port': 8065,
+        'basepath': '/api/v4',
+        # 'verify': True,
+        # 'mfa_token': 'YourMFAToken',
+        # 'auth': None,
+        'timeout': 30,
+        'request_timeout': None,
+        'debug': False,
+    })
