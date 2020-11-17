5b501dd0fb75cb629d7733573d117c51d749eed7
amirmohammad pirhosseinloo <amir.hosseinloo@cafebazaar.ir>
2020-11-17 04:38:56 +0330

    Add dockerfile for server.py

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+set -e
+
 python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+python manage.py test
 
 echo "Starting Gunicorn..."
-exec gunicorn ghararpanel.wsgi:application -w ${GUNICORN_WORKERS:-12} --bind 0.0.0.0:${SERVER_PORT:-8000}
+exec gunicorn mattermost_proxy.wsgi:application -w ${GUNICORN_WORKERS:-12} --bind 0.0.0.0:${SERVER_PORT:-8000}
