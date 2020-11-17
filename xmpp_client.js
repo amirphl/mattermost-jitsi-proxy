93a64152ef0ddb420f883b1ee742c1d5ea50ea41
amirmohammad pirhosseinloo <amir.hosseinloo@cafebazaar.ir>
2020-11-17 15:07:48 +0330

    Refactor xmpp client service

--- xmpp_client.js
+++ xmpp_client.js
@@ -21,7 +21,7 @@ function get_environment_variable(varname, defaultvalue) {
         return defaultvalue;
 }
 
-const random_string = function () {
+const generate_random_string = function () {
     const l = 5 + Math.floor(Math.random() * 5);
     const chars = "0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
     let str = "";
@@ -32,54 +32,49 @@ const random_string = function () {
     return str;
 };
 
-app.post('/messages', (req, res) => {
+// used to send a message to xmpp MUC
+app.post('/messages', async (req, res) => {
+    console.log('got a message ...');
     const {client, xml} = require("@xmpp/client");
-    const debug = require("@xmpp/debug");
     const xmpp = client({
         service: XMPP_SERVICE_URL,
         domain: XMPP_DOMAIN,
-        resource: random_string(),
+        resource: generate_random_string(),
         preferredSaslMechanism: "ANONYMOUS"
     });
 
-    xmpp.on("error", (err) => {
-    });
+    try {
+        await xmpp.start();
+    } catch (e) {
+        console.error(e);
+        res.status(500).send()
+    }
 
-    xmpp.on("offline", () => {
-    });
+    const conference_address = req.body.room_id + "@" + "conference." + XMPP_DOMAIN;
+    const user = req.body.user;
 
-    xmpp.on("stanza", async (stanza) => {
-        // if (stanza.is('message')) {
-        //     if (stanza.getChild('body') && stanza.getChild('body').text()) {
-        //         if (stanza.attrs.type === 'groupchat') {
-        //             console.log("client ", client_index, " received ", stanza.getChild('body').text());
-        //         }
-        //     }
-        // }
-    });
+    try {
 
-    xmpp.on("online", async (client_jid) => {
-        const conference_address = req.body.room_id + "@" + "conference." + XMPP_DOMAIN;
         await xmpp.send(xml("presence", {
-            from: client_jid,
-            to: conference_address + "/" + req.body.user
+            from: conference_address,
+            to: conference_address + "/" + user,
         }, xml("x", {xmlns: "http://jabber.org/protocol/muc"})));
-
         const message = xml("message", {
                 type: "groupchat",
                 to: conference_address
             },
             xml("body", {}, req.body.message),
         );
-        xmpp.send(message);
-        xmpp.stop();
-    });
-
-    xmpp.start().catch(console.error);
-    res.send({message: "ok"});
+        await xmpp.send(message);
+        console.log('message being sent');
+        await xmpp.stop();
+    } catch (e) {
+        console.error(e);
+        res.status(500).send();
+    }
+    res.status(201).send();
 });
 
-// make the server listen to requests
 app.listen(PORT, () => {
     console.log(`Server running at: http://0.0.0.0:${PORT}/`);
 });
